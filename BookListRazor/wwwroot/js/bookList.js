1def7e482692791b81dc4777a7729a9f4f5f0ff2
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-12 10:00:49 -0500

    adding bookList.js

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -14,7 +14,23 @@ function loadDataTable() {
         "columns": [
             {"data":"name","width":"30%"},
             {"data":"author","width":"30%"},
-            {"data":"isbn","width":"30%"},
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                        <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                            Edit
+                        </a>
+                        &nbsp;
+                        <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                            Delete
+                        </a>
+                    </div>
+                        
+                    `
+                }
+            }
         ]
     })
 }
