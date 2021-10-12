3ea80c32ff3989b6f3b238d7203e984190fa3201
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-12 09:52:39 -0500

    adding bookList.js

---
+++ BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            {"data":"name","width":"30%"},
+            {"data":"author","width":"30%"},
+            {"data":"isbn","width":"30%"},
+        ]
+    })
+}
