721b75fdbf032291e37b8a01fdc019089a572bfa
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-10-13 09:34:37 -0500

    adding bookList.js

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -12,24 +12,23 @@ function loadDataTable() {
             "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "30%" },
-            { "data": "author", "width": "30%" },
-            { "data": "isbn", "width": "30%" },
+            { "data": "name", "width": "20%" },
+            { "data": "author", "width": "20%" },
+            { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                        <a href="/BookList/Edit?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                        <a href="/BookList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:70px;'>
                             Edit
                         </a>
                         &nbsp;
-                        <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                        <a class='btn btn-danger text-white' style='cursor:pointer; width:70px;'
+                            onclick=Delete('/api/book?id='+${data})>
                             Delete
                         </a>
-                    </div>
-                        
-                    `
-                }, "width": "30%"
+                        </div>`;
+                }, "width": "40%"
             }
         ],
         "language": {
